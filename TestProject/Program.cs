752be12d36cb6b4c78760772da6a1fdf8f37e40c
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-23 23:59:24 +0100

    Simplifed

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,15 +1,5 @@
-﻿using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        int firstValue = 500;
-        int secondValue = 600;
-        int largerValue;
-
-        largerValue = Math.Max(firstValue, secondValue); // Assign the larger value using Math.Max
-
-        Console.WriteLine(largerValue); // Output: 600
-    }
-}
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
