ab2865bb25b72a00f4657278cda337622b3174fd
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-24 09:03:16 +0100

    Added first set of if statements

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -7,3 +7,12 @@ int roll3 = dice.Next(1, 7);
 int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total > 14)
+{
+    Console.WriteLine("You Win!");
+}
+if (total < 15)
+{
+    Console.WriteLine("You Lose, Try again!")
+}
